c2e3233f8c2c868fee4309830ebe340ca3cfa880
chait2605 <chait.2605@gmail.com>
2018-11-30 19:09:32 -0800

    made changes to makefile and other minor changes

--- backEnd/app.py
+++ backEnd/app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route('/chart')
 def chart():
     chart = {"Technology": 560}
-    return render_template('chart.html', chart = chart)
+    return render_template('chart.html', chart=chart)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
